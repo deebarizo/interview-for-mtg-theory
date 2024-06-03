34b61355e57097c9842aa6e2b8138f855214e21e
Deovanni "Dee" Barizo <deebarizo@gmail.com>
2024-06-03 11:27:22 +0800

    Add comments, create two expect results (one w extra credit

--- src/expectedResult__ExtraCredit.js
+++ src/expectedResult__ExtraCredit.js
@@ -1,13 +1,16 @@
 export default {
+  // Sort mainDeck and sideboard
+  // First, by type.includes("Land") and those without that type
+  // Second, by manaValue
+  // Third, by copies
   mainDeck: [
     {
-      copies: 1,
+      copies: 4,
       atomicCard: {
-        _id: "c",
-        name: "Khenra Spellspear",
-        type: "Creature",
-        manaValue: 2,
-        manaCost: "1R",
+        _id: "b",
+        name: "Copperline Gorge",
+        type: "Land",
+        manaValue: 0,
       },
     },
     {
@@ -16,25 +19,6 @@ export default {
         _id: "a",
         name: "Sokenzan, Crucible of Defiance",
         type: "Legendary Land",
-        manaValue: 0,
-      },
-    },
-    {
-      copies: 2,
-      atomicCard: {
-        _id: "d",
-        name: "Urabrask's Forge",
-        type: "Artifact",
-        manaValue: 3,
-        manaCost: "2R",
-      },
-    },
-    {
-      copies: 4,
-      atomicCard: {
-        _id: "b",
-        name: "Copperline Gorge",
-        type: "Land",
         manaValue: 0,
       },
     },
@@ -48,8 +32,16 @@ export default {
         manaCost: "1R",
       },
     },
-  ],
-  sideboard: [
+    {
+      copies: 1,
+      atomicCard: {
+        _id: "c",
+        name: "Khenra Spellspear",
+        type: "Creature",
+        manaValue: 2,
+        manaCost: "1R",
+      },
+    },
     {
       copies: 2,
       atomicCard: {
@@ -60,6 +52,8 @@ export default {
         manaCost: "2R",
       },
     },
+  ],
+  sideboard: [
     {
       copies: 4,
       atomicCard: {
@@ -68,6 +62,16 @@ export default {
         type: "Sorcery",
         manaValue: 1,
         manaCost: "R",
+      },
+    },
+    {
+      copies: 2,
+      atomicCard: {
+        _id: "d",
+        name: "Urabrask's Forge",
+        type: "Artifact",
+        manaValue: 3,
+        manaCost: "2R",
       },
     },
   ],
